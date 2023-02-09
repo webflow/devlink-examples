9ae335df644dfae4da5e48a80fff0f3adb41fbe3
Bryant Chou <bryant@webflow.com>
2023-02-09 12:47:19 -0800

    Fix InteractionsProvider

--- weather/pages/_app.tsx
+++ weather/pages/_app.tsx
@@ -2,14 +2,17 @@ import "../devlink/index.css";
 import "../devlink/global.css";
 
 import type { AppProps } from "next/app";
-import { InteractionsProvider, Layout, SideNav, TopNav } from "../devlink";
+
+import { InteractionsProvider, Layout, SideNav, TopNav, createIX2Engine } from "../devlink";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
+      <InteractionsProvider createEngine={createIX2Engine}>
       <Layout
         sidebar={<SideNav />}
         topnav={<TopNav />}
         main={<Component {...pageProps} />}
       />
+      </InteractionsProvider>
   );
 }
